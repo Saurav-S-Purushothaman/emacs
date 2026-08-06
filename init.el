f943bfcd63a902ed0e4457422da8f4b27763af92
Saurav-S-Purushothaman <saurav.kudajadri@gmail.com>
2026-08-07 00:24:23 +0530

    Add markdown mode

--- init.el
+++ init.el
@@ -137,3 +137,11 @@
 (use-package which-key
   :init
   (which-key-mode))
+
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "multimarkdown")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
